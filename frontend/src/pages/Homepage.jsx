39352d6406e59e5af33e045bac53055087879a66
neema-s <shrivastavaneema@gmail.com>
2025-07-15 18:52:07 +0530

    Add homepage layout with initial styling

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React,{useEffect,useState} from "react";
+import './homepage.css';
+function Homepage() {
+  return (
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  );
+}
+
+export default Homepage;
+
