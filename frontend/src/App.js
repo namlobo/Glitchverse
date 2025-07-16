bfec86b71e06457e3e5340af7307caf94be6fea1
Namritha Lobo <nd.lobo.05@gmail.com>
2025-07-16 18:07:08 +0530

    Resolve merge conflict in Login.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,44 @@
 // import logo from './logo.svg';
 import './App.css';
-import Homepage from './pages/Homepage';
+// import Homepage from './pages/Homepage';
 import { Routes, Route } from 'react-router-dom';
 
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//         <Routes>
+//           <Route path="/" element={<Homepage/>}></Route>
+//         </Routes>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import Homepage from './pages/Homepage';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+
+// Auth Context
+import { AuthProvider } from './context/authContext';
+
+// Protected Route Wrapper (optional)
+// import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <AuthProvider>
         <Routes>
-          <Route path="/" element={<Homepage/>}></Route>
+          {/* <Route path="/" element={<Signup />} /> */}
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Homepage />} />
+
         </Routes>
-      </header>
+      </AuthProvider>
     </div>
   );
 }
