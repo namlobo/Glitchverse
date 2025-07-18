31420243229b294debbe52637d256c4b04e541cd
neema-s <shrivastavaneema@gmail.com>
2025-07-18 11:51:51 +0530

    Resolving the disappearing cursor issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,43 @@
 import './App.css';
-
-import Homepage from './pages/Homepage';
-import Login from './pages/Login';
+import ImageGrid from './components/Game/MultiCursor';
+// import Homepage from './pages/Homepage';
+// import Login from './pages/Login';
 // import Signup from './pages/Signup';
 
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route /*, useLocation*/ } from 'react-router-dom';
 import { AuthProvider } from './context/authContext';
 
 function App() {
-  const location = useLocation();
-  const isHomepage =
-    location.pathname === '/' ||
-    location.pathname === '/home' ||
-    location.pathname === '/homepage';
+  // const location = useLocation();
+  // const isHomepage =
+  //   location.pathname === '/' ||
+  //   location.pathname === '/home' ||
+  //   location.pathname === '/homepage';
 
   return (
     <AuthProvider>
       <div className="App">
-        {isHomepage ? (
+        {/* {isHomepage ? (
           <header className="App-header">
             <Routes>
-              <Route path="/" element={<Homepage />} />
               <Route path="/home" element={<Homepage />} />
               <Route path="/homepage" element={<Homepage />} />
             </Routes>
           </header>
-        ) : (
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            {/* <Route path="/signup" element={<Signup />} /> */}
-          </Routes>
-        )}
+        ) : ( */}
+        <Routes>
+          <Route path="/" element={<ImageGrid />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/signup" element={<Signup />} /> */}
+        </Routes>
+        {/* )} */}
       </div>
     </AuthProvider>
   );
 }
 
 export default App;
+
+
+
+
